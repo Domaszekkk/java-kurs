322e3479b33ef94b008e14c5987d1070c6f50f85
Domaszekkk <mkorchut4@gmail.com>
2026-02-20 00:26:15 +0100

    +kalkulator prawie complited

--- calc/src/Calculator.java
+++ calc/src/Calculator.java
@@ -3,26 +3,39 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj pierwszą liczbę ");
-        Double a = scanner.nextDouble();
-        scanner.nextLine();
-        System.out.println("podaj operator ");
-        char operator = scanner.nextLine().charAt(0);
-        System.out.println("podaj drugą liczbę ");
-        Double b = scanner.nextDouble();
-        scanner.nextLine();
+        System.out.println("wprowadz równanie");
+        double a = scanner.nextDouble();
+        char operator = scanner.next().charAt(0);
+        double b = scanner.nextDouble();
         Double wynik = switch (operator) {
            case '+' -> a + b;
            case '-' -> a - b;
            case '*' -> a * b;
-           case '/' -> a / b;
-           case '%' -> a % b;
+           case '/' -> {
+               if (b == 0) {
+                   System.out.println("ndrd przez zero ");
+                   yield null;
+               }
+               yield a / b;
+           }
+           case '%' -> {
+               if (b == 0) {
+                   System.out.println("ndrd przez zero ");
+                   yield null;
+               }
+               yield a % b;
+           }
            case '^' -> Math.pow(a, b);
             default -> null;
         } ;
+        if (wynik == null) {
+            System.out.println("Nieznany operator");
+            return;
+        }
         System.out.printf("%s %s %s %n",a,operator,b);
         System.out.println(wynik);
         System.out.println(wynik > 0 ? "wynik jest dodatni " : "wynik jest ujemny ");
+        System.out.println(wynik %2 == 0 ? "wynik jest parzysty " : "wynik jest nieparzysty ");
     }
 
 }
