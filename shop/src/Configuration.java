f6b7043555ed5b35130a679ddd7c58f3c9e22c4a
Domaszekkk <mkorchut4@gmail.com>
2026-04-01 21:13:54 +0200

    +task1

---
+++ shop/src/Configuration.java
@@ -0,0 +1,23 @@
+public class Configuration {
+    private final ConfigurationType type;
+    private final String name;
+    private final double price;
+
+    public Configuration(ConfigurationType type, String name, double price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public ConfigurationType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
